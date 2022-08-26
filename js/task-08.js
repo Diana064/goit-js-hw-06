d05d184bf58021538aae44635aec6174e658c554
Diana064 <dipustolyakova02@gmail.com>
2022-08-26 16:03:56 +0200

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,36 @@
+// const forma = document.querySelector('.login-form');
+
+// function controlForm(event) {
+// 	event.preventDefault();
+// 	const formEl = event.currentTarget.elements;
+// 	const emailEl = formEl.email.value;
+// 	const passwordEl = formEl.password.value;
+
+// 	if (emailEl === '' || passwordEl === '') {
+// 		alert('Заповненні не всі поля!');
+// 	}
+// 	console.log('Ваша почта: ', emailEl);
+// 	console.log('Ваш пароль: ', passwordEl);
+// 	event.currentTarget.reset();
+// }
+// forma.addEventListener('submit', controlForm);
+
 const forma = document.querySelector('.login-form');
 
-function controlForm(event) {
+forma.addEventListener('submit', (event) => {
 	event.preventDefault();
-	const formEl = event.currentTarget.elements;
-	const emailEl = formEl.email.value;
-	const passwordEl = formEl.password.value;
-	if (emailEl === '' || passwordEl === '') {
-		alert('Заповненні не всі поля!');
+	const formData = {};
+
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === '' || password.value === '') {
+		return alert('Всі поля мають бути заповнені!');
 	}
-	console.log('Ваша почта: ', emailEl);
-	console.log('Ваш пароль: ', passwordEl);
-	event.currentTarget.reset();
-}
-forma.addEventListener('submit', controlForm);
+
+	formData[email.name] = email.value;
+	formData[password.name] = password.value;
+	console.log(formData);
+	forma.reset();
+});
