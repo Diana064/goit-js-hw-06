245e1f7d0cd9b22bd520697df0e1437d7cebfcc1
Diana064 <dipustolyakova02@gmail.com>
2022-08-26 16:04:56 +0200

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,3 @@
-// const forma = document.querySelector('.login-form');
-
-// function controlForm(event) {
-// 	event.preventDefault();
-// 	const formEl = event.currentTarget.elements;
-// 	const emailEl = formEl.email.value;
-// 	const passwordEl = formEl.password.value;
-
-// 	if (emailEl === '' || passwordEl === '') {
-// 		alert('Заповненні не всі поля!');
-// 	}
-// 	console.log('Ваша почта: ', emailEl);
-// 	console.log('Ваш пароль: ', passwordEl);
-// 	event.currentTarget.reset();
-// }
-// forma.addEventListener('submit', controlForm);
-
 const forma = document.querySelector('.login-form');
 
 forma.addEventListener('submit', (event) => {
