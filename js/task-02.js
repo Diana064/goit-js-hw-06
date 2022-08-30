2957492aad8c918794268a92bf39a3c38656a594
Diana064 <dipustolyakova02@gmail.com>
2022-08-30 14:50:27 +0200

    update 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,13 @@ const ingredients = [
 	'Condiments',
 ];
 const ulEl = document.querySelector('#ingredients');
-const elements = [];
-for (let i = 0; i < ingredients.length; i += 1) {
-	const ingredient = ingredients[i];
-	// console.log(ingredient);
-	const liEl = document.createElement('li');
 
-	liEl.textContent = ingredient;
-	// console.log(liEl);
+const elements = ingredients.map((ingredient) => {
+	const liEl = document.createElement('li');
 	liEl.classList.add('item');
-	elements.push(liEl);
-}
+	liEl.textContent = `${ingredient}`;
+	return liEl;
+});
+
 ulEl.append(...elements);
-console.log(ulEl);
+// console.log(ulEl);
